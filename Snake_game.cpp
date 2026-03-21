34f044020e29aca4cdc28b2d12880f51f802ddcf
Ibraheem-cpp <ibraheemtahir84@gmail.com>
2026-03-21 23:48:22 +0500

    Make Food Class

--- Snake_game.cpp
+++ Snake_game.cpp
@@ -1,27 +1,49 @@
 #include <iostream>
 #include "raylib.h"
+#include "raymath.h"
 
 using namespace std;
 
+const int cellSize = 25;
+const int cellCount = 30;
+
+class Food {
+private:
+    Vector2 pos;
+public:
+    Food() {
+        int x = GetRandomValue(0, cellCount - 1);
+        int y = GetRandomValue(0, cellCount - 1);
+        this->pos.x = x;
+        this->pos.y = y;
+    }
+
+    void draw() const {
+        int x = pos.x * cellSize;
+        int y = pos.y * cellSize;
+        DrawRectangle(x, y, cellSize, cellSize, ORANGE);
+    }
+};
+
 int main()
 {
-    const int width = 750;
-    const int height = 750;
+    const int width = cellSize * cellCount;
+    const int height = cellSize * cellCount;
 
     InitWindow(width, height, "Snake Game");
     SetTargetFPS(60);
 
+    Food food;
+
     while (!WindowShouldClose()) {
 
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawCircle(width / 2, height / 2, 25.0, RED);
+        ClearBackground(DARKGREEN);
+        food.draw();
         EndDrawing();
     }
 
-
     CloseWindow();
-        
         
     return 0;
 }
