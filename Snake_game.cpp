98212bbabf2335f75e14986d4c5ef981451bfa44
Ibraheem-cpp <ibraheemtahir84@gmail.com>
2026-03-24 12:55:46 +0500

    Moving the Snake

--- Snake_game.cpp
+++ Snake_game.cpp
@@ -11,6 +11,18 @@ const int cellCount = 25;
 class Snake {
 private:
     deque<Vector2> body;
+    Vector2 Direction = { 1,0 };
+    double lastUpdateTime = 0;
+
+    bool triggerEvent(double interval) {
+        double currentTime = GetTime();
+        if (currentTime - lastUpdateTime >= interval) {
+            lastUpdateTime = currentTime;
+            return true;
+        }
+        return false;
+    }
+
 public:
     Snake() {
         this->body = { Vector2{6,9}, Vector2{5,9}, Vector2{4,9} };
@@ -22,6 +34,13 @@ public:
             float pos_y = this->body[i].y * cellSize;
             Rectangle segment = Rectangle{ pos_x,pos_y,cellSize,cellSize };
             DrawRectangleRounded(segment, 0.6, 8, MAROON);
+        }
+    }
+
+    void Update() {
+        if (this->triggerEvent(0.15)) {
+            this->body.pop_back();
+            this->body.push_front({ this->body[0].x + this->Direction.x, this->body[0].y + this->Direction.y });
         }
     }
 };
@@ -67,6 +86,8 @@ int main()
     Snake snake;
 
     while (!WindowShouldClose()) {
+
+        snake.Update();
 
         BeginDrawing();
         ClearBackground(DARKGREEN);
